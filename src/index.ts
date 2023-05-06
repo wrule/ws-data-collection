63214ada3a18d1209d03da17db204925534613d0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-06 15:31:00 +0800

    复制代码

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+import { Console } from "console"
 
-console.log('你好，世界');
+const { WebsocketStream } = require('@binance/connector')
+const logger = new Console({ stdout: process.stdout, stderr: process.stderr })
+
+// define callbacks for different events
+const callbacks = {
+  open: () => logger.debug('Connected with Websocket server'),
+  close: () => logger.debug('Disconnected with Websocket server'),
+  message: (data: any) => logger.info(data)
+}
+
+const websocketStreamClient = new WebsocketStream({ logger, callbacks })
+// subscribe ticker stream
+websocketStreamClient.ticker('bnbusdt')
+// close websocket stream
+setTimeout(() => websocketStreamClient.disconnect(), 6000)
