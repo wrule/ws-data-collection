0aca5a4c60c5838a21b1b3260a86653726c2c04e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-06 16:13:25 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,23 @@ const callbacks = {
   open: () => console.log('Connected with Websocket server'),
   close: () => console.log('Disconnected with Websocket server'),
   message: (data: any) => {
-    const obj = JSON.parse(data);
-    console.log(obj.a, obj.A, obj.b, obj.B);
+    try {
+      const obj = JSON.parse(data);
+      if (obj.s) {
+        const list = [
+          Number(new Date()),
+          Number(obj.b), Number(obj.B), Number(obj.a), Number(obj.A),
+        ];
+        console.log(JSON.stringify(list) + ',');
+      }
+    } catch (e) {
+      console.log(e);
+    }
   },
 }
 
 const websocketStreamClient = new WebsocketStream({ callbacks });
 // subscribe ticker stream
-websocketStreamClient.ticker('btcusdt');
+websocketStreamClient.bookTicker('btcusdt');
 // close websocket stream
 // setTimeout(() => websocketStreamClient.disconnect(), 6000)
