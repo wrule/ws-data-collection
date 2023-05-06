381afbd0c0872775f5ed73b3008a9a148b72cdaa
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-06 15:44:14 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,14 @@ const logger = new Console({ stdout: process.stdout, stderr: process.stderr })
 const callbacks = {
   open: () => logger.debug('Connected with Websocket server'),
   close: () => logger.debug('Disconnected with Websocket server'),
-  message: (data: any) => logger.info(data)
+  message: (data: any) => {
+    const obj = JSON.parse(data);
+    console.log(obj.a, obj.A, obj.b, obj.B);
+  },
 }
 
 const websocketStreamClient = new WebsocketStream({ logger, callbacks })
 // subscribe ticker stream
-websocketStreamClient.ticker('bnbusdt')
+websocketStreamClient.ticker('btcusdt')
 // close websocket stream
-setTimeout(() => websocketStreamClient.disconnect(), 6000)
+// setTimeout(() => websocketStreamClient.disconnect(), 6000)
